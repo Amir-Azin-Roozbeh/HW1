6a442c27f5bf7419a6eefec3f313a839684cb498
amirhallaji <a.hallaji.b@gmail.com>
2021-11-04 23:26:01 +0330

    feat: commmit before sleeping

--- Q3/controllers/my_controller/my_controller.py
+++ Q3/controllers/my_controller/my_controller.py
@@ -1,5 +1,7 @@
 from controller import Robot, Motor, GPS, Compass
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 TIME_STEP = 64
 
@@ -9,7 +11,7 @@ WHEEL_RADIUS = 0.0205
 
 CHASIS_LENGTH = 0.052
 
-RADIUS = 0.25
+RADIUS = 0.125
 
 # -----------------------------------------------
 
@@ -26,6 +28,20 @@ def get_bearing(compass: Compass):
     north = compass.getValues()
     rad = math.atan2(north[0], north[1])
     return rad
+
+
+# ----------------------------------------------
+
+def go_circle(leftMotor, rightMotor):     
+    leftMotor.setVelocity(((RADIUS - 2 * CHASIS_LENGTH) * MAX_SPEED))
+    rightMotor.setVelocity(RADIUS * MAX_SPEED)
+
+# ----------------------------------------------
+
+def archimedean_spiral(leftMotor, rightMotor, angle):     
+    leftMotor.setVelocity((angle * RADIUS - 2 * CHASIS_LENGTH ) * MAX_SPEED)
+    rightMotor.setVelocity((RADIUS) * MAX_SPEED)
+
 
 # ----------------------------------------------
 
@@ -54,12 +70,16 @@ if __name__ == '__main__':
     y_axis = []
     theta = []
     
-     
-    leftMotor.setVelocity(((RADIUS - 2 * CHASIS_LENGTH) * MAX_SPEED))
-    rightMotor.setVelocity(RADIUS * MAX_SPEED)
+    # go_circle(leftMotor, rightMotor)
+    # archimedean_spiral(leftMotor, rightMotor)
+    co = 1
 
-    while robot.step(TIME_STEP) != -1 and counter != 100:
+    while robot.step(TIME_STEP) != -1 and counter != 1000:
         counter += 1
+        angle = get_bearing(compass)
+        print(angle)
+        archimedean_spiral(leftMotor, rightMotor, co)
+        co += math.pi/180
         
         gps_val = gps.getValues()
         x_axis.append(gps_val[0])
